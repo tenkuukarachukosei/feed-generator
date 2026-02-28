3e4571d1db10e2f5826573bd391149bf66a43f74
tenkuukarachukosei <titaniumucitizen@gmail.com>
2026-03-01 08:59:09 +0900

    Initial commit

---
+++ src/algos/whats-alf.ts
@@ -0,0 +1,42 @@
+import { QueryParams } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
+import { AppContext } from '../config'
+
+// max 15 chars
+export const shortname = 'whats-alf'
+
+export const handler = async (ctx: AppContext, params: QueryParams) => {
+  let builder = ctx.db
+    .selectFrom('post')
+    .selectAll()
+    .orderBy('indexedAt', 'desc')
+    .orderBy('cid', 'desc')
+    .limit(params.limit)
+
+  if (params.cursor) {
+    const timeStr = new Date(parseInt(params.cursor, 10)).toISOString()
+    builder = builder.where('post.indexedAt', '<', timeStr)
+  }
+  const res = await builder.execute()
+  const jp = /[ぁ‐んァ‐ヶ――‐鉞]/
+  
+
+  const feed = res
+.filter((row) => {
+const text = (row as any).text ?? ''
+return jp.test(text)
+})
+.map((row) => ({
+post: row.uri,
+}))
+
+  let cursor: string | undefined
+  const last = res.at(-1)
+  if (last) {
+    cursor = new Date(last.indexedAt).getTime().toString(10)
+  }
+
+  return {
+    cursor,
+    feed,
+  }
+}
